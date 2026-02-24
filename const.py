06bf398b0de46618d79cb4736ae4ead8dafe5d44
reskfa <yangfasker@gmail.com>
2026-02-24 14:55:39 +0800

    Create const.py

---
+++ const.py
@@ -0,0 +1,4 @@
+wind_available = False            # 没有 Wind 设为 False                                  
+ts_token = 'your_tushare_token'   # tushare.pro 去申请，贴进来                                                        
+jqdata_username = ''                                                                      
+jqdata_password = ''
